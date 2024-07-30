319515945d81ae2f7a5b012bdb99e38501640eea
Sabir <alphadev@M1.local>
2024-07-30 19:33:18 +0600

    add responsiveness: home grid view

--- src/components/BooksGallery/BooksGallery.jsx
+++ src/components/BooksGallery/BooksGallery.jsx
@@ -5,7 +5,7 @@ const BooksGallery = ({ books }) => {
     return (
         <div className="mt-16">
             <h2 className="text-center text-4xl font-playfair font-semibold">Books</h2>
-            <div className="grid grid-cols-3 gap-6 mt-6">
+            <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-6 mt-6">
                 {
                     books.map((book, index) => <BooksGalleryTile key={index} book={book}></BooksGalleryTile>)
                 }
